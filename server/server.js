6bb5844b93b705c8788561983b8e27a569964265
deftonechris <deftonechris@msn.com>
2022-10-05 17:50:04 -0600

    added type defs, resolvers, connected our db, added auth, played with api

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { ApolloServer } = require("apollo-server-express");
 const { authMiddleware } = require("./utils/auth");
+const colors = require("colors");
 require("dotenv").config();
 
 const path = require("path");
