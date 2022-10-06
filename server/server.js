d434bbcd6548f4548052b8d83029a3532f88de6b
deftonechris <deftonechris@msn.com>
2022-10-05 21:04:21 -0600

    set up monogoose models and added missing packages

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ if (process.env.NODE_ENV === "production") {
 }
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  res.sendFile(path.join(__dirname, "../client/"));
 });
 
 const server = new ApolloServer({
