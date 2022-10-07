75f307aaecdd182b5a73bbaffe93f169bd42911d
Lreyes4 <103380089+Lreyes4@users.noreply.github.com>
2022-10-07 14:52:43 -0600

    added styling and div for account info

--- client/src/pages/homePage.js
+++ client/src/pages/homePage.js
@@ -10,22 +10,35 @@ const HomePage = ({ ...otherProps }) => {
     <div className="min-h-full bg-gray-100 font-body">  
       <div className="h-screen flex flex-col ">
         <Header />
+        <div className="
+          relative flex item-center justify-center h-full
+          bg-cover bg-center bg-fixed bg-no-repeat
+        ">
+          <h3 class="font-medium leading-tight text-3xl mt-0 mb-2 text-blue-600">Account Information</h3>
+          <div class="flex justify-center">
+            <ul class="bg-white rounded-lg border border-gray-200 w-96 text-gray-900">
+              <li class="px-6 py-2 border-b border-gray-200 w-full rounded-t-lg">Name</li>
+              <li class="px-6 py-2 border-b border-gray-200 w-full">Username</li>
+              <li class="px-6 py-2 border-b border-gray-200 w-full">Email</li>
+              <li class="px-6 py-2 border-b border-gray-200 w-full">A fourth item</li>
+              <li class="px-6 py-2 w-full rounded-b-lg">And a fifth one</li>
+            </ul>
+          </div>
 
-
-
-
-
-
-
-
-
-
-
-
+      
+        
+        
+        
+        
+        </div>
       </div>
       <Footer />
     </div> //end wrapper for the whole page
   ) 
 };
 
+
+HomePage.defaultProps = {
+  heading: "",
+};
 export default HomePage;
