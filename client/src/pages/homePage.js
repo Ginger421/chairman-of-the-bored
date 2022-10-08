141ada9f4f7abd90b6cfd26e179e26b31932c33e
Ginger Owen <gingerowen0421@gmail.com>
2022-10-08 11:34:11 -0600

    adjusted image

--- client/src/pages/homePage.js
+++ client/src/pages/homePage.js
@@ -8,23 +8,10 @@ const HomePageLayout = ({ ...otherProps }) => {
 
     //wrapper for the whole page
     <div className="min-h-full bg-gray-100 font-body">  
-      <div className="h-screen flex flex-col ">
-        <Header />
         <div className="
           relative flex item-center justify-center h-full
           bg-cover bg-center bg-fixed bg-no-repeat
         ">
-<<<<<<< HEAD
-=======
-          <h3 class="font-medium leading-tight text-3xl mt-0 mb-2 text-blue-600">Account Information</h3>
-          <div class="flex justify-center">
-            <ul class="bg-white rounded-lg border border-gray-200 w-96 text-gray-900">
-              <li class="px-6 py-2 border-b border-gray-200 w-full rounded-t-lg">Name</li>
-              <li class="px-6 py-2 border-b border-gray-200 w-full">Username</li>
-              <li class="px-6 py-2 border-b border-gray-200 w-full">Email</li>
-            </ul>
-          </div> 
->>>>>>> 0beee3db99c1004da9549001bb1b5e11d8146104
 
           <div class="flex flex-wrap justify-center">
             <img
@@ -34,7 +21,6 @@ const HomePageLayout = ({ ...otherProps }) => {
           />
           </div>
 
-<<<<<<< HEAD
           <h3 class="font-medium leading-tight text-3xl mt-0 mb-2 text-blue-600">Account Information</h3>
           <div class="flex justify-center">
             <ul class="bg-white rounded-lg border border-gray-200 w-96 text-gray-900">
@@ -43,28 +29,14 @@ const HomePageLayout = ({ ...otherProps }) => {
               <li class="px-6 py-2 border-b border-gray-200 w-full">Email</li>
             </ul>
           </div>        
-=======
-
-
-      
-        
-        
-        
->>>>>>> 0beee3db99c1004da9549001bb1b5e11d8146104
         
         </div>
-      </div>
-      <Footer />
     </div> //end wrapper for the whole page
   ) 
 };
 
 
-<<<<<<< HEAD
-HomePage.defaultProps = {
-=======
 HomePageLayout.defaultProps = {
->>>>>>> 0beee3db99c1004da9549001bb1b5e11d8146104
   heading: "",
 };
 export default HomePageLayout;
