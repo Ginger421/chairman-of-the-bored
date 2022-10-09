8f5195d7beecf8ccb8ac9c14bf118c49d8a07bd6
V Mendez <victormendez990@gmail.com>
2022-10-09 02:12:09 -0600

    Styling and Page layout

--- client/src/components/BoredApi.js
+++ client/src/components/BoredApi.js
@@ -2,13 +2,13 @@ import React from 'react';
 
 const UserApi = (props) => {
     return (
-        <div className='z-10 flex justify-center items-center content-center h-screen'>
+        <div className='z-10 flex justify-center items-center content-center'>
             <div className='flex flex-col bg-sky-400 justify-center items-center h-96 max-w-lg w-full animate-pulse rounded-lg'>
-
-                <h1 className='flex p-10 text-4xl text-center'>What are you going to do today? </h1>
+                <form className='flex flex-col p-8 '>
+                    <h1 className='flex p-10 text-4xl text-center'>What are you going to do today? </h1>
             
-                <button className='bg-yellow-300 h-10 animate-bounce shadow-2xl rounded-lg p-4 flex items-center hover:text-sky-400'>Tell Me!</button>
-
+                    <button className='bg-yellow-300 h-10 animate-bounce shadow-2xl rounded-lg p-6 flex place-self-center items-center w-32 hover:text-sky-400'>Tell Me!</button>
+                </form>
             </div>
         </div>
     )
@@ -21,7 +21,7 @@ const GenericApi = (props) => {
 
                 <h1 className='flex p-10 text-4xl text-center'>What are you going to do today? </h1>
             
-                <button className='bg-yellow-300 h-10 animate-bounce shadow-2xl rounded-lg p-4 flex items-center hover:text-sky-400'>Tell Me!</button>
+                <button className='bg-yellow-300 h-10 animate-bounce shadow-2xl rounded-lg p-6 flex items-center hover:text-sky-400'>Tell Me!</button>
 
             </div>
         </div>
@@ -37,7 +37,7 @@ const BoredApi = (props) => {
         )
     } else {
         return (
-            <GenericApi/>
+            <UserApi/>
         )
     }
 }
