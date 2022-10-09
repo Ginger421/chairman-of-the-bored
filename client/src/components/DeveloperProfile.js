1e7ebb693e4f92ccb8b68b7b5a6f5341b04ce04b
V Mendez <victormendez990@gmail.com>
2022-10-08 21:54:50 -0600

    Started About page and Dev component

---
+++ client/src/components/DeveloperProfile.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const DevProfile = (props) => {
+    <div>
+        <div>
+            <img 
+            src=""
+            class="p-1 bg-white border rounded max-w-sm"
+            alt="Developer Headshot"/>
+        </div>
+   
+        <div>
+            <ul>
+                <li>Name:</li>
+                <li><a href=''>GitHub</a></li>
+                <li><a href=''>LinkedIn</a></li>
+            </ul>
+        </div> 
+    </div>
+}
+
+export default DevProfile;
--- client/src/pages/aboutPage.js
+++ client/src/pages/aboutPage.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import DevProfile from '../components/DeveloperProfile';
 import EveryPageLayout from '../layouts/EveryPage';
 
 const AboutPage = () => {
     <EveryPageLayout>
-        
+        <div className='flex flex-col'>
+            <div>
+                <h1>About our App!</h1>
+                <p></p>
+            </div>
+
+            <div>
+                <h1><a href='https://github.com/Ginger421/chairman-of-the-bored'>Visit our GitHub!</a></h1>
+            </div>
+
+            <div>
+                <h1>Meet the Developers behind your favorite App!</h1>
+                <DevProfile/>
+                <DevProfile/>
+                <DevProfile/>
+                <DevProfile/>
+            </div>
+        </div>
     </EveryPageLayout>
 };
 
