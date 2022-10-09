0477addbb5291b38189d1f7f8bfdbb4a68e9a7cc
V Mendez <victormendez990@gmail.com>
2022-10-09 02:56:14 -0600

    Logged in Home Page Edits

--- client/src/components/BoredApi.js
+++ client/src/components/BoredApi.js
@@ -5,9 +5,18 @@ const UserApi = (props) => {
         <div className='z-10 flex justify-center items-center content-center'>
             <div className='flex flex-col bg-sky-400 justify-center items-center h-96 max-w-lg w-full animate-pulse rounded-lg'>
                 <form className='flex flex-col p-8 '>
-                    <h1 className='flex p-10 text-4xl text-center'>What are you going to do today? </h1>
-            
-                    <button className='bg-yellow-300 h-10 animate-bounce shadow-2xl rounded-lg p-6 flex place-self-center items-center w-32 hover:text-sky-400'>Tell Me!</button>
+                    <div>
+                        <h1 className='flex p-2 text-4xl text-center'>What are you going to do today? </h1>
+                    </div>
+                    <div className='flex flex-col p-2'>
+                        <label for="quantity">How many participants:</label>
+                        <input type="number" className='p-2' id="quantity" name="quantity" min="1" max="99"></input>
+                        <label for="vol">Price (from free to $$$): </label>
+                        <input type="range" id="vol" name="vol" min="0" max="10"></input>
+                    </div>
+                    <div className='flex flex-col p-6'>
+                        <button className='bg-yellow-300 h-10 animate-bounce shadow-2xl rounded-lg p-6 flex place-self-center items-center w-32 hover:text-sky-400'>Tell Me!</button>
+                    </div>    
                 </form>
             </div>
         </div>
