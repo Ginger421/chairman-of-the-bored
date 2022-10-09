c203bca33b792e51631e7b13d89289863be09e72
Ginger Owen <gingerowen0421@gmail.com>
2022-10-08 22:44:10 -0600

    created account info component

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,7 @@ const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<HomePage/>}/>
+        <Route path="/" element={<ProfilePage/>}/>
         <Route path="/about" element={<AboutPage/>}/>
         <Route path="/profile" element={<ProfilePage/>}/>
       </Routes>
---
+++ client/src/components/AccountInfo.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const AccountInfo = () => {
+    return (
+        <div>
+        <h3 className="font-medium leading-tight text-3xl mt-0 mb-2 text-blue-600">Account Information</h3>
+        <div className="flex flex-wrap justify-center">
+          <img
+          src="../"
+          className="p-1 bg-blue border rounded max-w-sm"
+          alt="User's profile picture"
+        />
+        </div>
+
+        <ul className="bg-white rounded-lg border border-gray-200 w-96 text-gray-900">
+          <li className="px-6 py-2 border-b border-gray-200 w-full rounded-t-lg">Name</li>
+          <li className="px-6 py-2 border-b border-gray-200 w-full">Username</li>
+          <li className="px-6 py-2 border-b border-gray-200 w-full">Email</li>
+        </ul>
+      </div>        
+    
+    )
+}
